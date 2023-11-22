5920e997b1c424509fe8437e8ddd49f4494c41ee
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-11-22 10:47:00 +0100

    Preparing release 1.0
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,8 +2,8 @@ Changelog
 =========
 
 
-1.0a1 (unreleased)
-------------------
+1.0 (2023-11-22)
+----------------
 
 - Initial release.
   [erral]
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="codesyntax.login",
-    version="1.0a1",
+    version="1.0",
     description="Add-on on to set login basics for CodeSyntax users",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
