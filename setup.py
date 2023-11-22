cd470c598b23ba65f94d8f0300e6e3e711a8c2ce
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-11-22 09:50:10 +0100

    add deps

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-long_description = '\n\n'.join([
-    open('README.rst').read(),
-    open('CONTRIBUTORS.rst').read(),
-    open('CHANGES.rst').read(),
-])
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CONTRIBUTORS.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
 
 
 setup(
-    name='codesyntax.login',
-    version='1.0a1',
+    name="codesyntax.login",
+    version="1.0a1",
     description="Add-on on to set login basics for CodeSyntax users",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
@@ -31,45 +33,43 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='Python Plone CMS',
-    author='Mikel Larreategi',
-    author_email='mlarreategi@codesyntax.com',
-    url='https://github.com/collective/codesyntax.login',
+    keywords="Python Plone CMS",
+    author="Mikel Larreategi",
+    author_email="mlarreategi@codesyntax.com",
+    url="https://github.com/collective/codesyntax.login",
     project_urls={
-        'PyPI': 'https://pypi.python.org/pypi/codesyntax.login',
-        'Source': 'https://github.com/collective/codesyntax.login',
-        'Tracker': 'https://github.com/collective/codesyntax.login/issues',
+        "PyPI": "https://pypi.python.org/pypi/codesyntax.login",
+        "Source": "https://github.com/collective/codesyntax.login",
+        "Tracker": "https://github.com/collective/codesyntax.login/issues",
         # 'Documentation': 'https://codesyntax.login.readthedocs.io/en/latest/',
     },
-    license='GPL version 2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['codesyntax'],
-    package_dir={'': 'src'},
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["codesyntax"],
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
     python_requires=">=3.7",
     install_requires=[
-        'setuptools',
+        "setuptools",
         # -*- Extra requirements: -*-
-        'z3c.jbot',
-        'plone.api>=1.8.4',
-        'plone.app.dexterity',
+        "plone.api>=1.8.4",
+        "pas.plugins.oidc",
+        "collective.regenv",
     ],
     extras_require={
-        'test': [
-            'plone.app.testing',
+        "test": [
+            "plone.app.testing",
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
+            "plone.testing>=5.0.0",
+            "plone.app.contenttypes",
+            "plone.app.robotframework[debug]",
         ],
     },
     entry_points="""
     [z3c.autoinclude.plugin]
     target = plone
-    [console_scripts]
-    update_locale = codesyntax.login.locales.update:update_locale
     """,
 )
